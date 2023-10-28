b2050882e2a9060b93b05d82db86ad0dbba01414
U. Mariano Gálvez <umg@labqtz.local>
2023-10-28 08:45:12 -0600

    First Commit

---
+++ src/prueba/pkg3/Prueba3.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba.pkg3;
+
+/**
+ *
+ * @author umg
+ */
+public class Prueba3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+           System.out.println("Hello Word");
+           
+    }
+    
+}
